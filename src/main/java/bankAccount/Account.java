4cb0f5281ac50c9eeaaec9641850aed5991a513e
damiang <damiangildea12@gmail.com>
2024-10-11 11:50:05 +0100

    Working on bank account class

---
+++ src/main/java/bankAccount/Account.java
@@ -0,0 +1,63 @@
+package bankAccount;
+
+public class Account {
+    private double balance;
+    private int accountNumber;
+    private String name;
+    private String email;
+    private int phoneNumber;
+
+    public void depositFunds(double input) {
+        balance += input;
+        System.out.println("Deposit Successful! New balance = " + balance);
+    }
+
+    public void withdrawFunds(double input) {
+        if (balance - input < 0) {
+            System.out.println("Not enough funds!!! - " + balance);
+        } else {
+            balance -= input;
+            System.out.println("Withdrawal successfull! New balance = " + balance);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/bankAccount/Main.java
@@ -0,0 +1,12 @@
+package bankAccount;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello");
+    }
+
+}
