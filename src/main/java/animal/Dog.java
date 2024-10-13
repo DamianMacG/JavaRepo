e67545168576a94cd47fe57eaf899e353a4147fc
damiang <damiangildea12@gmail.com>
2024-10-13 13:22:42 +0100

    Adding in specific dog attributes

--- src/main/java/animal/Dog.java
+++ src/main/java/animal/Dog.java
@@ -1,10 +1,30 @@
 package animal;
 
 public class Dog extends Animal {
-//private String earShape;
-//private String
+    private String earShape;
+    private String tailShape;
 
     public Dog() {
         super("Mutt", "Biggish", 75);
+
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), type, weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
     }
 }
--- src/main/java/animal/Main.java
+++ src/main/java/animal/Main.java
@@ -6,8 +6,14 @@ public class Main {
         Animal animal = new Animal("Generic Animal", "Huge", 200);
         doAnimalStuff(animal, "slow");
 
-        Dog dog = new Dog();
-        doAnimalStuff(dog, "fast");
+//        Dog dog = new Dog();
+//        doAnimalStuff(dog, "fast");
+
+        Dog doggo = new Dog("Doggo", 15);
+        doAnimalStuff(doggo, "fast");
+
+        Dog retriever = new Dog("Lab", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
